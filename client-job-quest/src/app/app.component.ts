0433f113c3e9e71604e3304c4eaceefec4279ed8
Tharun <tharun.adithya@desk365.io>
2023-12-16 11:45:33 +0530

    material inclusions

--- client-job-quest/src/app/app.component.ts
+++ client-job-quest/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
